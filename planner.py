c18b3bf965f79bec5f0571aaa65a3af1e54b3010
deviant-rat <sharovdanny01@gmail.com>
2025-12-26 19:25:40 +0300

    Issue with adding doubles fixed

--- planner.py
+++ planner.py
@@ -1,46 +1,51 @@
 todo_list= []
 id = 0
+double = False
+found = False
 while True:
     print("Waiting for your command")
     user_command = input(">")
     if (user_command == "add"):
+        double = False
         name = input("Please, input the name \n >")
-        if id == 0:
-            item = {
-                    "ID": id,
-                    "Name": name,
-                    "Status": False
-            }
-            id += 1
-            todo_list.append(item)
-            continue
         for item in todo_list:
             if item["Name"] == name:
-                print("Item with this name alredy exists.")
-                continue
-            else:
-                item = {
-                    "ID": id,
-                    "Name": name,
-                    "Status": False
-                }
-                id += 1
-                todo_list.append(item)
+                double = True
+        if double == True:        
+            print("Item with this name alredy exists.")
+        else:
+            item = {
+                "ID": id,
+                "Name": name,
+                "Status": False
+            }                    
+            id += 1
+            todo_list.append(item)
+            print("Item added")         
     elif(user_command == "help"):
         print("1. add - adds new item, \n help 2. list - shows all items, \n 3. help - shows a list of commands \n 4. done - changes item Status to done \n 5. exit - closes the programm")
     elif(user_command == "list"):
-        for item in todo_list:
-            if item["Status"] == True:
-                print("[x]", end=" ")
-            else:
-                print("[ ]", end=" ")
-            print(item["ID"], end=" ")
-            print(item["Name"])
+        if id == 0:
+            print('No items found')
+        else:
+            for item in todo_list:
+                if item["Status"] == True:
+                    print("[x]", end=" ")
+                else:
+                    print("[ ]", end=" ")
+                print(item["ID"], end=" ")
+                print(item["Name"])
     elif(user_command == "done"):
         name = input("Please, input the name \n >")
+        found = False
         for item in todo_list:
             if item["Name"] == name:
                 item["Status"] = True
+                found = True
+        if found == True:
+            print("Status changed successfuly")
+        else:
+            print("Item not found")
     elif(user_command == "exit"):
         print("Goodbye!")
         break
