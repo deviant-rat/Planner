7951751f0c00fd4f0cd426d2fadc2105a51ea441
deviant-rat <sharovdanny01@gmail.com>
2025-12-26 19:10:56 +0300

    small fixes

--- planner.py
+++ planner.py
@@ -5,18 +5,37 @@ while True:
     user_command = input(">")
     if (user_command == "add"):
         name = input("Please, input the name \n >")
-        item = {
-            "ID": id,
-            "Name": name,
-            "Status": False
-        }
-        id += 1
-        todo_list.append(item)
+        if id == 0:
+            item = {
+                    "ID": id,
+                    "Name": name,
+                    "Status": False
+            }
+            id += 1
+            todo_list.append(item)
+            continue
+        for item in todo_list:
+            if item["Name"] == name:
+                print("Item with this name alredy exists.")
+                continue
+            else:
+                item = {
+                    "ID": id,
+                    "Name": name,
+                    "Status": False
+                }
+                id += 1
+                todo_list.append(item)
     elif(user_command == "help"):
         print("1. add - adds new item, \n help 2. list - shows all items, \n 3. help - shows a list of commands \n 4. done - changes item Status to done \n 5. exit - closes the programm")
     elif(user_command == "list"):
         for item in todo_list:
-            print(item)
+            if item["Status"] == True:
+                print("[x]", end=" ")
+            else:
+                print("[ ]", end=" ")
+            print(item["ID"], end=" ")
+            print(item["Name"])
     elif(user_command == "done"):
         name = input("Please, input the name \n >")
         for item in todo_list:
@@ -25,3 +44,5 @@ while True:
     elif(user_command == "exit"):
         print("Goodbye!")
         break
+    else:
+        print("Command not found. For the full list of commands use 'help'")
